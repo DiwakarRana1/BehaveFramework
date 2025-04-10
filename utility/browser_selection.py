75f5fa7a379c466f2a6ed02a78535a820bb038d7
Diwakar Rana <diwakar.rana@cloudsufi.com>
2025-04-10 10:25:58 +0530

    Report New

--- utility/browser_selection.py
+++ utility/browser_selection.py
@@ -8,10 +8,9 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 def open_new_window(driver):
     """Opens a new browser window only if necessary."""
-    if len(driver.window_handles) == 1:  # Ensure only one window is open before creating another
+    if len(driver.window_handles) == 1:
         print("Opening a new browser window...")
         driver.switch_to.new_window('window')
-
 
 def get_driver(browser_name):
     if browser_name.lower() == "chrome":
@@ -24,19 +23,29 @@ def get_driver(browser_name):
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-gpu")
-        options.add_argument("--headless=new")  # Run Chrome in headless mode for CI/CD
+        options.add_argument("--headless=new")  # Uncomment for headless CI/CD
 
-        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+        driver = webdriver.Chrome(
+            service=ChromeService(ChromeDriverManager().install()),
+            options=options
+        )
+        driver.maximize_window()
 
     elif browser_name.lower() == "firefox":
         options = webdriver.FirefoxOptions()
 
-        # Check if geckodriver is installed
         geckodriver_path = shutil.which("geckodriver")
         if geckodriver_path:
-            driver = webdriver.Firefox(service=FirefoxService(geckodriver_path), options=options)
+            driver = webdriver.Firefox(
+                service=FirefoxService(geckodriver_path),
+                options=options
+            )
         else:
-            driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+            driver = webdriver.Firefox(
+                service=FirefoxService(GeckoDriverManager().install()),
+                options=options
+            )
+        driver.maximize_window()
 
     else:
         raise ValueError(f"Unsupported browser: {browser_name}")
--- utility/config.py
+++ utility/config.py
@@ -5,8 +5,8 @@ URLS = { # Add other URLs as needed
 }
 
 USER_DATA = {
-    "Full Name": "Diwakar",
-    "Email": "diwakar@google.com",
-    "Current Address": "abcd",
-    "Permanent Address": "abcdef"
+    "Full Name": "John Doe",
+    "Email": "johndoe@google.com",
+    "Current Address": "Street 400 Cuscowilla Dr City/Town Eatonton State/Province/Region Georgia Zip/Postal Code 31024 Phone Number (706) 484-2044 Country United States Latitude 33.411657Longitude-83.258892",
+    "Permanent Address": "Street 400 Cuscowilla Dr City/Town Eatonton State/Province/Region Georgia Zip/Postal Code 31024 Phone Number (706) 484-2044 Country United States Latitude 33.411657Longitude-83.258892"
 }
